ecf94a5b080147fd2214344854d8fd3937ddefdc
Ling Chunyang <laurenceling@outlook.com>
2018-04-03 14:40:54 +0800

    calculate node weight and sort subgraph instead of randomly pick

--- src/main/java/cn/edu/pku/sei/intellide/graph/qa/code_search/MySubgraph.java
+++ src/main/java/cn/edu/pku/sei/intellide/graph/qa/code_search/MySubgraph.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class MySubgraph {
+    double rootWeightSum = 0;
+    Set<Long> selectedRoot = new HashSet<>();
     Set<Long> nodes = new HashSet<>();
     Set<Long> edges = new HashSet<>();
     public void print(){

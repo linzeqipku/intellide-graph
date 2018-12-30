cedee9c2f9e88cf0dabab770ca839c774b9dcc24
Ying Qian <qianyingqy@pku.edu.cn>
2018-12-30 16:16:09 +0800

    modify TaskExtractor

--- src/main/java/cn/edu/pku/sei/intellide/graph/extraction/task/graph/FrequentGraphMiner.java
+++ src/main/java/cn/edu/pku/sei/intellide/graph/extraction/task/graph/FrequentGraphMiner.java
@@ -1,5 +1,6 @@
 package cn.edu.pku.sei.intellide.graph.extraction.task.graph;
 
+import cn.edu.pku.sei.intellide.graph.extraction.task.TaskExtractor;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
@@ -7,6 +8,7 @@ import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.CoreMap;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.neo4j.cypher.internal.frontend.v2_3.ast.functions.Str;
 import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
@@ -69,8 +71,10 @@ public class FrequentGraphMiner {
     // 为了保留frequency信息，只能用Fragment来存
 
     private static List<GraphInfo> obj = new ArrayList<>();
+    private static Map<Long, String> questionMap;
+    private static Map<Long, String> answerMap;
 
-    public static GraphDatabaseService db = new GraphDatabaseFactory().newEmbeddedDatabase(new File("E:/graphs/graph-POI"));
+//    public static GraphDatabaseService db = new GraphDatabaseFactory().newEmbeddedDatabase(new File("E:/graphs/graph-lucene"));
 
     public static void reset(String _libraryName) {
         for (Fragment<NodeInfo, Integer> fragment : frequentSubgraphs) {
@@ -83,24 +87,26 @@ public class FrequentGraphMiner {
     }
 
     public static void main(String[] args) {
-        mineGraphOfLibrary();
+//        setQuestionMap(getNodesFromNeo4j(Label.label("StackOverflowQuestion")));
+//        setAnswerMap(getNodesFromNeo4j(Label.label("StackOverflowAnswer")));
+        mineGraph();
     }
 
-    public static Map<Long,String> getNodesFromNeo4j(Label label) {
-        Map<Long,String> textMap = new LinkedHashMap<Long,String>();
-        try(Transaction tx = db.beginTx()){
-            ResourceIterator<org.neo4j.graphdb.Node> nodes = db.findNodes(label);
-            while(nodes.hasNext()){
-                org.neo4j.graphdb.Node node = nodes.next();
-                String text = (String)node.getProperty("body");
-                if(!text.equals("")){
-                    textMap.put(node.getId(),text);
-                }
-            }
-            tx.success();
-        }
-        return textMap;
-    }
+//    public static Map<Long,String> getNodesFromNeo4j(Label label) {
+//        Map<Long,String> textMap = new LinkedHashMap<Long,String>();
+//        try(Transaction tx = db.beginTx()){
+//            ResourceIterator<org.neo4j.graphdb.Node> nodes = db.findNodes(label);
+//            while(nodes.hasNext()){
+//                org.neo4j.graphdb.Node node = nodes.next();
+//                String text = (String)node.getProperty("body");
+//                if(!text.equals("")){
+//                    textMap.put(node.getId(),text);
+//                }
+//            }
+//            tx.success();
+//        }
+//        return textMap;
+//    }
 
     public static List<String> splitText(String text) {
         Properties props = new Properties();
@@ -115,50 +121,9 @@ public class FrequentGraphMiner {
         }
         return sentencesText;
     }
-    public static void parseNode(long id) {
-        org.neo4j.graphdb.Node textNode = db.getNodeById(id);
-        String body = textNode.getProperty("body").toString();
-        Document doc = Jsoup.parse(body);
-        doc.select("code").remove();
-        String text = doc.text();
-        List<String> sentences = splitText(text);
-        for (String sentence: sentences) {
-            Tree tree = NLPParser.parseGrammaticalTree(sentence);
-            PhraseInfo[] verbPhrases = PhraseExtractor.extractVerbPhrases(tree);
-            for (PhraseInfo phraseInfo : verbPhrases) {
-                PhraseFilter.filter(phraseInfo, sentence);
-                if (phraseInfo.getProofScore() > 0) {
-                    Tree stemmedPhraseTree = Stemmer.stemTree(Tree.valueOf(phraseInfo.getSyntaxTree()));
-                    VerbalPhraseStructureInfo vp = new VerbalPhraseStructureInfo(stemmedPhraseTree);
-                    GraphInfo graphInfo = GraphBuilder.buildGraphInfoFromStructure(vp);
-                    obj.add(graphInfo);
-                    Graph<NodeInfo, Integer> g = GraphBuilder.convertToParsemisGraph(graphInfo);
-                    Iterator<Node<NodeInfo, Integer>> ite = g.nodeIterator();
-                    while (ite.hasNext()) {
-                        Node<NodeInfo, Integer> n = ite.next();
-                    }
-                }
-            }
-        }
-    }
 
-    public static void mineGraphOfLibrary() {
-        Map<Long, String> questionMap = getNodesFromNeo4j(Label.label("StackOverflowQuestion"));
-        Map<Long, String> answerMap = getNodesFromNeo4j(Label.label("StackOverflowAnswer"));
-        try(Transaction tx = db.beginTx()){
-            for (Map.Entry<Long, String> entry : questionMap.entrySet()) {
-				if (obj.size() > 500)
-					break;
-                parseNode(entry.getKey());
-            }
-            for (Map.Entry<Long, String> entry : answerMap.entrySet()) {
-				if (obj.size() > 1000)
-					break;
-                parseNode(entry.getKey());
-            }
-            tx.success();
-        }
-
+    public static HashSet<String> mineGraph() {
+        HashSet<String> phrases = new HashSet<>();
         if (obj != null && obj instanceof List<?>) {
             System.out.println("******************************************************************");
             System.out.println(obj.size());
@@ -183,17 +148,18 @@ public class FrequentGraphMiner {
             List<GraphInfo> frequentSubgraphsFromFilter = FrequentGraphPostRunner.filter(libraryName);
             System.out.println("frequentSubgraphsFromFilter");
             System.out.println(frequentSubgraphsFromFilter.size());
-            int cnt = 0;
             for (GraphInfo graphInfo: frequentSubgraphsFromFilter) {
                 VerbalPhraseStructureInfo vp = GraphParser.parseGraphToPhraseStructure(graphInfo);
                 if (vp != null) {
-                    System.out.println(vp);
-                    cnt++;
+                    phrases.add(vp.toNLText());
+                    System.out.println(vp.toNLText());
                 }
             }
             System.out.println("*************************");
-            System.out.println(cnt);
+            System.out.println(phrases.size());
+            System.out.println(phrases);
         }
+        return phrases;
     }
 
     public static List<Fragment<NodeInfo, Integer>> mineFrequentGraph() {
@@ -211,7 +177,7 @@ public class FrequentGraphMiner {
         settings.naturalOrderedNodeLabels = true;
         settings.algorithm = new de.parsemis.algorithms.gSpan.Algorithm<>();
         settings.strategy = new ThreadedDFSStrategy<>(16, new Statistics());
-        settings.minFreq = new IntFrequency(5);// iTextPdf项目只能用3，否则内存就不够了
+        settings.minFreq = new IntFrequency(4);// iTextPdf项目只能用3，否则内存就不够了
         settings.factory = new ListGraph.Factory<>(new NodeInfoLabelParser(), new IntegerLabelParser());
 
         Collection<Fragment<NodeInfo, Integer>> result = Miner.mine(graphs, settings);
@@ -244,6 +210,14 @@ public class FrequentGraphMiner {
     public static void setFrequentSubgraphs(List<Fragment<NodeInfo, Integer>> frequentSubgraphs) {
         FrequentGraphMiner.frequentSubgraphs = frequentSubgraphs;
     }
+
+    public static void setQuestionMap(Map<Long, String> questionMap) { FrequentGraphMiner.questionMap = questionMap; }
+
+    public static void setAnswerMap(Map<Long, String> answerMap) { FrequentGraphMiner.answerMap = answerMap; }
+
+    public static void addToObj(GraphInfo graphInfo) { obj.add(graphInfo); }
+
+    public static int getObjSize() { return obj.size(); }
 }
 
 class NodeInfoLabelParser implements LabelParser<NodeInfo> {
